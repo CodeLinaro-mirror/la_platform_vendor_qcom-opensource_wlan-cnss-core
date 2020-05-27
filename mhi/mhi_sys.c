49cdf019ab7e222b92cba4b6efdc5205aa0b0c24
Hao Tian <haotian@codeaurora.org>
2020-05-27 20:09:34 +0800

    change mhi log to error
    change mhi log from verbose to error, then it produce
    less print information to not effect throughput.
    
    Change-Id: I6b3ac51761f6530ab3c904d5bcfb81d460b18459

--- mhi/mhi_sys.c
+++ mhi/mhi_sys.c
@@ -18,7 +18,7 @@
 
 #include "mhi_sys.h"
 
-enum MHI_DEBUG_LEVEL mhi_msg_lvl = MHI_MSG_VERBOSE;
+enum MHI_DEBUG_LEVEL mhi_msg_lvl = MHI_MSG_ERROR;
 
 #ifdef CONFIG_MSM_MHI_DEBUG
 enum MHI_DEBUG_LEVEL mhi_ipc_log_lvl = MHI_MSG_VERBOSE;
