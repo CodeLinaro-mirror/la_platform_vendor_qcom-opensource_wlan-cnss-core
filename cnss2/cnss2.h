c7de11c8f935b7f7082a93d98df2aef5f39458e1
Ben Wang <benw@codeaurora.org>
2020-06-24 20:28:47 +0800

    cnss2: Expose APIs to prevent and allow PCIe L1 state
    WLAN driver can use the APIs to control if PCIe can enter L1 and
    L1ss states.
    
    Change-Id: Ida87038023e0489f8af099c7840cd1350c10613a

--- cnss2/cnss2.h
+++ cnss2/cnss2.h
@@ -209,9 +209,18 @@ extern int cnss_wlan_register_driver(struct cnss_wlan_driver *driver);
 extern void cnss_wlan_unregister_driver(struct cnss_wlan_driver *driver);
 extern void cnss_device_crashed(struct device *dev);
 #ifdef CONFIG_PCI_MSM
+extern int cnss_pci_prevent_l1(struct device *dev);
+extern void cnss_pci_allow_l1(struct device *dev);
 extern int cnss_pci_link_down(struct device *dev);
 extern int cnss_wlan_pm_control(struct device *dev, bool vote);
 #else /* CONFIG_PCI_MSM */
+static inline int cnss_pci_prevent_l1(struct device *dev)
+{
+	return 0;
+}
+static inline void cnss_pci_allow_l1(struct device *dev)
+{
+}
 static inline int cnss_pci_link_down(struct device *dev)
 {
 	return 0;
