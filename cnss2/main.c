f577365bbed2f60b1590640d3f11ca8105a203ae
Bing Sun <quic_bings@quicinc.com>
2022-09-30 09:23:32 +0800

    cnss2: fix cnss_request_pm_qos build error
    cpu_latency_qos_* is not defined in old kernel, so use pm_qos_* if
    it is not defined
    
    Change-Id: I5ed61132357da668471127ec8e1343e0fffe3779

--- cnss2/main.c
+++ cnss2/main.c
@@ -323,6 +323,7 @@ int cnss_get_platform_cap(struct device *dev, struct cnss_platform_cap *cap)
 }
 EXPORT_SYMBOL(cnss_get_platform_cap);
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 7, 0))
 void cnss_request_pm_qos(struct device *dev, u32 qos_val)
 {
 	struct cnss_plat_data *plat_priv = cnss_bus_dev_to_plat_priv(dev);
@@ -344,6 +345,30 @@ void cnss_remove_pm_qos(struct device *dev)
 	cpu_latency_qos_remove_request(&plat_priv->qos_request);
 }
 EXPORT_SYMBOL(cnss_remove_pm_qos);
+#else
+void cnss_request_pm_qos(struct device *dev, u32 qos_val)
+{
+	struct cnss_plat_data *plat_priv = cnss_bus_dev_to_plat_priv(dev);
+
+	if (!plat_priv)
+		return;
+
+	pm_qos_add_request(&plat_priv->qos_request, PM_QOS_CPU_DMA_LATENCY,
+			   qos_val);
+}
+EXPORT_SYMBOL(cnss_request_pm_qos);
+
+void cnss_remove_pm_qos(struct device *dev)
+{
+	struct cnss_plat_data *plat_priv = cnss_bus_dev_to_plat_priv(dev);
+
+	if (!plat_priv)
+		return;
+
+	pm_qos_remove_request(&plat_priv->qos_request);
+}
+EXPORT_SYMBOL(cnss_remove_pm_qos);
+#endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 7, 0)) */
 
 int cnss_wlan_enable(struct device *dev,
 		     struct cnss_wlan_enable_cfg *config,
