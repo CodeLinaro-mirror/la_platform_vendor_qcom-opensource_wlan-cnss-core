8fc349c8882aaa3b628beb16a15602429e6b513d
Nijun Gong <quic_ngong@quicinc.com>
2022-11-14 11:13:33 +0800

    cnss2: Allow QCA6174 to bypass power off after PCIE enumeration
    Allow QCA6174 to bypass power off after PCIE enumeration.
    And move configurations to other place to make code clean.

--- cnss2/pci.c
+++ cnss2/pci.c
@@ -6124,6 +6124,7 @@ static bool cnss_should_suspend_pwroff(struct pci_dev *pci_dev)
 	switch (pci_dev->device) {
 	case QCA6390_DEVICE_ID:
 	case QCA6490_DEVICE_ID:
+	case QCA6174_DEVICE_ID:
 		suspend_pwroff = false;
 		break;
 	default:
