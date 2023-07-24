37beb00a9dcef44c06293d1274cd54a3dcf1a638
Zhiwei Yang <quic_zhiwyang@quicinc.com>
2023-07-24 17:15:39 +0800

    cnss2: Add sysfs support for configuring timer sync interval
    It can be used to tune the host and fw time synchronization interval
    for better accuracy.
    
    Change-Id: I0a3d8895520cec526341f278c91f153dd0bd1966
    CRs-Fixed: 3257702

--- cnss2/bus.h
+++ cnss2/bus.h
@@ -70,4 +70,6 @@ int cnss_bus_get_iova_ipa(struct cnss_plat_data *plat_priv, u64 *addr,
 int cnss_bus_set_therm_cdev_state(struct cnss_plat_data *plat_priv,
 				  unsigned long thermal_state,
 				  int tcdev_id);
+int cnss_bus_update_time_sync_period(struct cnss_plat_data *plat_priv,
+				     unsigned int time_sync_period);
 #endif /* _CNSS_BUS_H */
