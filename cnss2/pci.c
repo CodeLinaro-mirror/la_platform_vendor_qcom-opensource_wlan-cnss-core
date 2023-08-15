545558065ef66ca9d1d913d1393ab7f698fc7903
Zhiwei Yang <quic_zhiwyang@quicinc.com>
2023-08-15 14:16:13 +0800

    cnss2: Always collect host dump info
    Always collect host dump info for debug.
    
    Change-Id: Ib5a271a52e54d821775f0651aa2c9a5f0579847c

--- cnss2/pci.c
+++ cnss2/pci.c
@@ -5739,21 +5739,12 @@ void cnss_pci_clear_dump_info(struct cnss_pci_data *pci_priv)
 
 void cnss_pci_device_crashed(struct cnss_pci_data *pci_priv)
 {
-	struct cnss_plat_data *plat_priv;
-
 	if (!pci_priv) {
 		cnss_pr_err("pci_priv is NULL\n");
 		return;
 	}
 
-	plat_priv = pci_priv->plat_priv;
-	if (!plat_priv) {
-		cnss_pr_err("plat_priv is NULL\n");
-		return;
-	}
-
-	if (plat_priv->recovery_enabled)
-		cnss_pci_collect_host_dump_info(pci_priv);
+	cnss_pci_collect_host_dump_info(pci_priv);
 	cnss_device_crashed(&pci_priv->pci_dev->dev);
 }
 
