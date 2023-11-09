ada149c53ba5863bc4013aa4ad573c8035b280cb
Brandon Huang <quic_yanghuan@quicinc.com>
2023-11-09 18:04:14 +0800

    cnss2: Add mhi config for pcie switch case
    Add new mhi config for wlan chip attached pcie switch. Disable bw_scale
    and satellite in mhi config in this case.
    
    Change-Id: Ib2d48495dc124b5bc30c228d17bbd1b2aef008f8

--- cnss2/pci.c
+++ cnss2/pci.c
@@ -443,6 +443,24 @@ static const struct mhi_controller_config cnss_mhi_config_no_satellite = {
 	.m2_no_db = true,
 };
 
+#ifdef CONFIG_PCIE_SWITCH_NTN3
+#define CNSS_MHI_BUS_MISC_EVT_COUNT 1
+static const struct mhi_controller_config cnss_mhi_config_pcie_switch_ntn3 = {
+	.max_channels = 32,
+	.timeout_ms = 10000,
+	.use_bounce_buf = false,
+	.buf_len = 0x8000,
+	.num_channels = ARRAY_SIZE(cnss_mhi_channels) -
+			CNSS_MHI_SATELLITE_CH_CFG_COUNT,
+	.ch_cfg = cnss_mhi_channels,
+	.num_events = ARRAY_SIZE(cnss_mhi_events) -
+			CNSS_MHI_SATELLITE_EVT_COUNT -
+			CNSS_MHI_BUS_MISC_EVT_COUNT,
+	.event_cfg = cnss_mhi_events,
+	.m2_no_db = true,
+};
+#endif
+
 static struct cnss_pci_reg ce_src[] = {
 	{ "SRC_RING_BASE_LSB", CE_SRC_RING_BASE_LSB_OFFSET },
 	{ "SRC_RING_BASE_MSB", CE_SRC_RING_BASE_MSB_OFFSET },
@@ -6583,6 +6601,11 @@ static int cnss_pci_register_mhi(struct cnss_pci_data *pci_priv)
 		else
 			cnss_mhi_config = &cnss_mhi_config_no_satellite;
 	}
+
+#ifdef CONFIG_PCIE_SWITCH_NTN3
+	if (pci_priv->pcie_switch_ntn3)
+		cnss_mhi_config = &cnss_mhi_config_pcie_switch_ntn3;
+#endif
 
 	mhi_ctrl->tme_supported_image = cnss_is_tme_supported(pci_priv);
 
