3b57230a76ed1bb7f7c3db83fac6b90b285ccf5d
gongx <gongx@qti.qualcomm.com>
2026-06-01 17:35:26 +0800

    wlan-cnss-core: update code with latest linux head file
    Change-Id: I45ad35628a5fb7059a21a7fda57bd078d24755ed

--- mhi/host/boot.c
+++ mhi/host/boot.c
@@ -409,15 +409,6 @@ void mhi_fw_load_handler(struct mhi_controller *mhi_cntrl)
 	if (ret)
 		dev_err(dev, "Could not capture serial number via BHI\n");
 
-	for (i = 0; i < ARRAY_SIZE(mhi_cntrl->oem_pk_hash); i++) {
-		ret = mhi_read_reg(mhi_cntrl, mhi_cntrl->bhi, BHI_OEMPKHASH(i),
-				   &mhi_cntrl->oem_pk_hash[i]);
-		if (ret) {
-			dev_err(dev, "Could not capture OEM PK HASH via BHI\n");
-			break;
-		}
-	}
-
 	/* wait for ready on pass through or any other execution environment */
 	if (!MHI_FW_LOAD_CAPABLE(mhi_cntrl->ee))
 		goto fw_load_ready_state;
